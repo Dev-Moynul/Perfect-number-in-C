f8177f9395db70a63bc1f81a296bfb93967b640c
Dev-Moynul <stu.moynulislam@gmail.com>
2025-11-11 18:26:50 +0600

    Perfect number

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i=1,n,sum=0;
+   printf("Enter the number: ");
+   scanf("%d",&n);
+   while(i<n)
+   {
+       if(n%i==0)
+        sum=sum+i;
+       i++;
+   }
+   if(sum==n)
+    printf("%d is a perfect number",i);
+   else
+    printf("%d is not a perfect number",i);
+    return 0;
+}
